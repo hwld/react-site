92fa8a2e91deef95bc924e5d154254a36d24a070
hwld <0701hw@gmail.com>
2020-06-27 22:13:58 +0900

    テストを追加

---
+++ src/components/project/ui/ContentColumn.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from '../../../test-util';
+import ContentColumn from './ContentColumn';
+
+describe('<ContentColumn>', () => {
+  test('Content,Footerが正しく表示される', () => {
+    const { queryByText } = render(
+      <ContentColumn footerMenu={<div>testFooter</div>}>
+        <div>testContent</div>
+      </ContentColumn>,
+    );
+    expect(queryByText('testContent')).toBeTruthy();
+    expect(queryByText('testFooter')).toBeTruthy();
+  });
+});
